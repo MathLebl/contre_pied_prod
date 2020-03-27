2a282b24335730d60311f094181de08153348a0e
MathLebl <leblond.mathieu.kesemale@gmail.com>
2020-03-27 09:15:29 +0100

    dashboard admin set step 2

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,16 +1,16 @@
-ActiveAdmin.register Artist do
+ActiveAdmin.register User do
 
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
   # Uncomment all parameters which should be permitted for assignment
   #
-  permit_params :name, :description, :style, :image, :video, :deezer, :spotify, :youtube, :insta, :twitter, :slug, :user_id
+  permit_params :email, :encrypted_password, :reset_password_token, :reset_password_sent_at, :remember_created_at, :admin
   #
   # or
   #
   # permit_params do
-  #   permitted = [:name, :description, :style, :image, :video, :deezer, :spotify, :youtube, :insta, :twitter, :slug, :user_id]
+  #   permitted = [:email, :encrypted_password, :reset_password_token, :reset_password_sent_at, :remember_created_at, :admin]
   #   permitted << :other if params[:action] == 'create' && current_user.admin?
   #   permitted
   # end
