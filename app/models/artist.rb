fed063cd31ee563feeba87bbaf5eb8758becefba
MathLebl <leblond.mathieu.kesemale@gmail.com>
2020-03-28 15:24:33 +0100

    adminArtist fixed, validations seed ready

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -3,5 +3,4 @@ class Artist < ApplicationRecord
   has_many :events
 
   validates :name, presence: true, uniqueness: true
-  validates :category, inclusion: { in: %w(concert spectacle enfant),  message: "%{value} is not a valid size"}
 end
