d004cb521b47bf32776a0fe07fbbcb59db38e1ee
MathLebl <leblond.mathieu.kesemale@gmail.com>
2020-03-28 14:40:24 +0100

    controllers routes et vus ok

---
+++ app/controllers/artists_controller.rb
@@ -0,0 +1,27 @@
+class ArtistsController < ApplicationController
+  before_action :find_artist, only: [ :show ]
+  def index
+    @artists = Artist.all
+  end
+
+  def show
+  end
+
+  def enfant
+    @artists = Artist.where(category: "enfant")
+  end
+
+  def spectacle
+     @artists = Artist.where(category: "spectacle")
+  end
+
+  def concert
+     @artists = Artist.where(category: "artists")
+  end
+
+  private
+
+  def find_artist
+    @artist = Artist.find(params[:id])
+  end
+end
