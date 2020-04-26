559a24cc5df6c2497fd6c93492db6434aeb5cfd7
MathLebl <leblond.mathieu.kesemale@gmail.com>
2020-04-26 18:17:44 +0200

    Seeds Updated 3 artists inactif créés

--- db/seeds/artists-seed.rb
+++ db/seeds/artists-seed.rb
@@ -1,7 +1,7 @@
 require 'faker'
 
 puts 'Creating 10 fake Théatre Artists...'
-10.times do
+9.times do
   artist = Artist.new(
     name:    Faker::Movies::StarWars.character,
     description: Faker::Lorem.paragraph,
@@ -12,9 +12,19 @@ puts 'Creating 10 fake Théatre Artists...'
   )
   artist.save!
 end
+artist = Artist.new(
+  name:    Faker::Movies::StarWars.character,
+  description: Faker::Lorem.paragraph,
+  style: Faker::Movies::StarWars.planet,
+  category:  "Theatre",
+  user_id: 1,
+  active: false,
+  image: "https://images.unsplash.com/photo-1571173069043-82a7a13cee9f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+)
+artist.save!
 
 puts 'Creating 10 fake Concert Artists...'
-10.times do
+9.times do
   artist = Artist.new(
     name:    Faker::Music.band,
     description: Faker::Lorem.paragraph,
@@ -25,9 +35,19 @@ puts 'Creating 10 fake Concert Artists...'
   )
   artist.save!
 end
+artist = Artist.new(
+  name:    Faker::Movies::StarWars.character,
+  description: Faker::Lorem.paragraph,
+  style: Faker::Movies::StarWars.planet,
+  category:  "Concert",
+  user_id: 1,
+  active: false,
+  image: "https://images.unsplash.com/photo-1571173069043-82a7a13cee9f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+)
+artist.save!
 
 puts 'Creating 10 fake Jeune Publique Artists...'
-10.times do
+9.times do
   artist = Artist.new(
     name:    Faker::Movies::HarryPotter.character,
     description: Faker::Lorem.paragraph,
@@ -38,6 +58,16 @@ puts 'Creating 10 fake Jeune Publique Artists...'
   )
   artist.save!
 end
+artist = Artist.new(
+  name:    Faker::Movies::StarWars.character,
+  description: Faker::Lorem.paragraph,
+  style: Faker::Movies::StarWars.planet,
+  category:  "Jeune Publique",
+  user_id: 1,
+  active: false,
+  image: "https://images.unsplash.com/photo-1571173069043-82a7a13cee9f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+)
+artist.save!
 
 puts "Artists done"
 
