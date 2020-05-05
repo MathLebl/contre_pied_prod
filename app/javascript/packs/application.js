f96b9d373e28e97e8ec3add466d64e680f5f6761
MathLebl <leblond.mathieu.kesemale@gmail.com>
2020-05-06 00:28:52 +0200

    je lacherais pas

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,9 +4,9 @@ require("channels")
 
 import "bootstrap";
 import Swup from "swup";
+import { toggleBtn } from '/list-info-toggle.js';
 
 // Swup Initiation
 const swup = new Swup();
 
-import { toggleBtn } from 'list-info-toggle.js';
 
