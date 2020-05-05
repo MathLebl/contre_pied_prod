c0636294115b728c83101736d46cdd5a6176c2ff
MathLebl <leblond.mathieu.kesemale@gmail.com>
2020-05-05 20:52:34 +0200

    mise à jour yéshé

--- app/javascript/packs/list-info-toggle.js
+++ app/javascript/packs/list-info-toggle.js
@@ -1,9 +1,11 @@
 const buttons = document.querySelectorAll('.info-list > li > button')
 const commentsBlocks = document.querySelectorAll('.info-list > li > p')
+console.log(buttons)
+console.log(commentsBlocks)
 
 buttons.forEach((button, index) => {
   const commentsBlock = commentsBlocks[index];
   button.addEventListener('click', () => {
-    commentsBlock.classList.toggle('is-visible');
+    commentsBlock.classList.toggle('is-invisible');
   });
 });
