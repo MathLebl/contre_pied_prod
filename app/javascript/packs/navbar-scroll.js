ab7156124e03f174838c7797cd8a98804e2e5125
MathLebl <leblond.mathieu.kesemale@gmail.com>
2020-05-06 14:18:46 +0200

    navbar effect done

--- app/javascript/packs/navbar-scroll.js
+++ app/javascript/packs/navbar-scroll.js
@@ -1,9 +1,20 @@
 window.onscroll = function() {scrollFunction()};
+var navbar = document.querySelector(".navbar");
+var titre = navbar.querySelector("h4");
+var links = navbar.querySelectorAll(".nav-link");
+console.log(links)
 function scrollFunction() {
   if (document.body.scrollTop > 400 || document.documentElement.scrollTop > 400) {
-    document.querySelector(".navbar").style.backgroundColor = "#DBD5CD";
+    navbar.style.backgroundColor = "#FFEE93";
+    titre.style.color = "black";
+    links.forEach((link) => {
+      link.style.color = "black";
+    });
   } else {
-    document.querySelector(".navbar").style.backgroundColor = "transparent";
+    navbar.style.backgroundColor = "transparent";
+    titre.style.color = "#FFEE93";
+    links.forEach((link) => {
+      link.style.color = "#FFEE93";
+    });
   }
 }
-
