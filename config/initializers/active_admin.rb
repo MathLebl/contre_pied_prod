05baca32e9a8305fd79afc488fff8df8d68777c4
MathLebl <leblond.mathieu.kesemale@gmail.com>
2020-05-12 17:09:52 +0200

    back to site fixed

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -236,7 +236,7 @@ ActiveAdmin.setup do |config|
   #
     config.namespace :admin do |admin|
       admin.build_menu :utility_navigation do |menu|
-        menu.add label: "Retour au site", url: "http://localhost:3000/"
+        menu.add label: "Retour au site", url: "https://contre-pied-prod.herokuapp.com/"
         admin.add_logout_button_to_menu menu
       end
     end
