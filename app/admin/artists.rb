779cfa2864820acdd234febfd65bb51d2739fd41
MathLebl <leblond.mathieu.kesemale@gmail.com>
2020-06-18 15:49:59 +0200

    List Info FONCTIONNE

--- app/admin/artists.rb
+++ app/admin/artists.rb
@@ -1,5 +1,5 @@
 ActiveAdmin.register Artist do
-permit_params :name, :description, :style, :image, :video, :deezer, :spotify, :youtube, :insta, :twitter, :slug, :user_id, :active, :banner, :dice1, :dice2, :dice3, :dice4, :facebook, :soundcloud, :distribution, :credits, :presse, :partenaires, :category, :press_file
+permit_params :name, :description, :style, :image, :video, :deezer, :spotify, :youtube, :insta, :twitter, :slug, :user_id, :active, :banner, :dice1, :dice2, :dice3, :dice4, :facebook, :soundcloud, :category, :press_file, distribution: [], credits: [], presse: [], partenaires: []
   menu priority: 4
 
   action_item :publish, only: :show do
@@ -24,7 +24,35 @@ permit_params :name, :description, :style, :image, :video, :deezer, :spotify, :y
   filter :category
   filter :style
   filter :active, as: :check_boxes
+
+  form do |f|
+    f.inputs do
+      f.input :name
+      f.input :description, :as => :pagedown_text
+      f.input :style
+      f.input :image
+      f.input :video
+      f.input :deezer
+      f.input :spotify
+      f.input :youtube
+      f.input :insta
+      f.input :twitter
+      f.input :user_id
+      f.input :active
+      f.input :banner
+      f.input :dice1
+      f.input :dice2
+      f.input :dice3
+      f.input :dice4
+      f.input :facebook
+      f.input :soundcloud
+      f.input :distribution
+      f.input :credits
+      f.input :presse
+      f.input :partenaires
+      f.input :category
+      f.input :press_file
+      f.actions
+    end
+  end
 end
-
-
-
