33017345f7aee566af836c6ef64e169c91d67895
David <davidobob@laposte.net>
2020-06-22 14:32:10 +0200

    creation of some methods to make the code more readable

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,21 +1,16 @@
 class OrdersController < ApplicationController
   def create
+    # Lignes 4 & 5 servent pour la partie active admin pour l'instant, à modifier plus tard
     product = Product.find(params[:product_id])
     order  = Order.create!(product: product, product_name: product.name, amount: product.price, state: 'pending', user: current_user)
-    items = session[:cart]
-    items.each do |element|
-      Item.create!(product_id: element["id"], order_id: order.id)
-    end
-    line_items = items.map do |item|
-      {
-        name: item["name"],
-        images: [item["photo_url"]],
-        amount: item["price_cents"],
-        currency: 'eur',
-        quantity: 1
-      }
-    end
 
+    items = session[:cart]
+
+    # Création des modèles Items pour chaque produit dans la commande, et récupération des infos pour la variable line_items
+    create_items_objects(items, order)
+    line_items = set_line_items(items)
+
+    # Lancement de la phase de paiement Stripe
     session = Stripe::Checkout::Session.create(
       payment_method_types: ['card'],
       line_items: line_items,
@@ -31,4 +26,36 @@ class OrdersController < ApplicationController
     @order = current_user.orders.find(params[:id])
   end
 
+  private
+
+  def create_items_objects(items, order)
+    items.each do |element|
+      Item.create!(product_id: element["id"], order_id: order.id)
+    end
+  end
+
+  def set_line_items(items)
+    # Si le cart n'est pas vide, on rempli line_items avec ses infos
+    unless items.empty?
+      items.map do |item|
+        {
+          name: item["name"],
+          images: [item["photo_url"]],
+          amount: item["price_cents"],
+          currency: 'eur',
+          quantity: 1
+        }
+      end
+      # s'il est vide, on rempli line_items avec les infos du produit de la page sur laquelle on a appuyé sur "purchase"
+    else
+      product = Product.find(params[:product_id])
+      [{
+        name: product.name,
+        images: [product.photo_url],
+        amount: product.price_cents,
+        currency: 'eur',
+        quantity: 1
+      }]
+    end
+  end
 end
