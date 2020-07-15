3aeea1f8bba63632da8b6e7b144a11f07bb0a50f
MathLebl <leblond.mathieu.kesemale@gmail.com>
2020-07-15 15:10:03 +0200

    Code cleaned

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -4,16 +4,13 @@ class EventsController < ApplicationController
 
   def index
     @cities = Event.select(:city).group(:city).collect{|e| e.city}
-    # @artists = Event.select(:artist_id).group(:artist_id).collect{|e| e.artist.name}
     @artists = Artist.all
     @dates = Event.select(:date).group(:date).order('date ASC').collect{|e| e.date}
     if params[:city].present?
       @events = Event.all.order('date ASC').select { |event| event.city == params[:city]}
     elsif params[:artist_id].present?
-      # @events = Event.all.order('date ASC').select { |event| event.artist == params[:artist_id]}
       @events = Event.where(artist_id: params[:artist_id])
     elsif params[:date].present?
-       # @events = Event.all.order('date ASC').where { |event| event.date == params[:date]}
       @events = Event.where(date: params[:date])
     else
       @events = Event.all.order('date ASC')
