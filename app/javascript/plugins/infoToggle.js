b3bce208da0b2d8219076ca8221f66f82146da30
David <davidobob@laposte.net>
2020-07-20 16:57:51 +0200

    removed old code

--- app/javascript/plugins/infoToggle.js
+++ app/javascript/plugins/infoToggle.js
@@ -1,12 +1,6 @@
 const buttons = document.querySelectorAll('.info-list > li > button')
 const commentsBlocks = document.querySelectorAll('.info-text')
 const toggleBtn = () => {
-  // buttons.forEach((button, index) => {
-  //   const commentsBlock = commentsBlocks[index];
-  //   button.addEventListener('click', () => {
-  //     commentsBlock.classList.toggle('is-visible');
-  //   });
-  // });
   buttons.forEach((button, index) => {
     document.addEventListener('click', (e) => {
       if (e.target === button) {
