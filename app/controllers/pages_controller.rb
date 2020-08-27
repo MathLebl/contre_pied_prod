aa5d98acb4c6bc040d31f3af130f1ed6c4a64a7d
David <davidobob@laposte.net>
2020-08-27 16:47:54 +0200

    implemented other payment events

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,6 +7,57 @@ class PagesController < ApplicationController
     @events = events.first(8)
     actus = Actu.all
     @actus = actus.first(4)
+
+    machin = {
+      "id": "evt_1HKlR6J0As8BjpLS6YD8hTuY",
+      "object": "event",
+      "api_version": "2020-03-02",
+      "created": 1598535999,
+      "data": {
+        "object": {
+          "id": "pi_1HKlQWJ0As8BjpLS6frwzy4h",
+          "object": "payment_intent",
+          "amount": 3600,
+          "amount_capturable": 0,
+          "amount_received": 3600,
+          "application": nil,
+          "application_fee_amount": nil,
+          "canceled_at": nil,
+          "cancellation_reason": nil,
+          "capture_method": "automatic",
+          "charges": {
+            "object": "list",
+            "data": [
+              {
+                "id": "ch_1HKlR5J0As8BjpLSZfzdoHTl",
+                "object": "charge",
+                "amount": 3600,
+                "amount_refunded": 0,
+                "application": nil,
+                "application_fee": nil,
+                "application_fee_amount": nil,
+                "balance_transaction": "txn_1HKlR5J0As8BjpLSVaBXydx3",
+                "billing_details": {
+                  "address": {
+                    "city": nil,
+                    "country": "FR",
+                    "line1": nil,
+                    "line2": nil,
+                    "postal_code": nil,
+                    "state": nil
+                  },
+                  "email": "tototo@laposte.net",
+                  "name": "tototo",
+                  "phone": nil
+                }
+              }
+            ]
+          }
+        }
+      }
+    }
+    puts "_________________-"
+    puts machin[:data][:object][:charges][:data][0][:billing_details][:email]
   end
 
   def legals
@@ -47,11 +98,19 @@ class PagesController < ApplicationController
     case event.type
     when 'payment_intent.succeeded'
       payment_intent = event.data.object # contains a Stripe::PaymentIntent
-      puts "________________________________________"
-      puts 'PaymentIntent was successful!'
-    when 'payment_method.attached'
-      payment_method = event.data.object # contains a Stripe::PaymentMethod
-      puts 'PaymentMethod was attached to a Customer!'
+      user = User.find_by(email: event[:data][:object][:charges][:data][0][:billing_details][:email])
+      last_order = user.orders.last
+      last_order.state = "Payé"
+    when 'payment_intent.canceled'
+      payment_intent = event.data.object # contains a Stripe::PaymentIntent
+      user = User.find_by(email: event[:data][:object][:charges][:data][0][:billing_details][:email])
+      last_order = user.orders.last
+      last_order.state = "Annulé"
+    when 'payment_intent.payment_failed'
+      payment_intent = event.data.object # contains a Stripe::PaymentIntent
+      user = User.find_by(email: event[:data][:object][:charges][:data][0][:billing_details][:email])
+      last_order = user.orders.last
+      last_order.state = "Echec"
     # ... handle other event types
     else
       # Unexpected event type
