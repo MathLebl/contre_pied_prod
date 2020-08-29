8fa305d56a3c4f6f3ea5f0e053ab5db42a6c3349
MathLebl <leblond.mathieu.kesemale@gmail.com>
2020-08-29 03:07:03 +0200

    orders_controller bug fixed again and again and again and again

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -3,13 +3,13 @@ class OrderMailer < ApplicationMailer
   def order_send(order)
     @order = order
     @user = user
-    mail_to: user.email, subject: "Votre commande numéro #{ order.id } a été envoyée"
+    mail to: user.email, subject: "Votre commande numéro #{ order.id } a été envoyée"
   end
 
   def order_paid(order)
     @order = order
     @user = user
-    mail_to: user.email, subject: "Votre commande numéro #{ order.id } a été prise en compte"
+    mail to: user.email, subject: "Votre commande numéro #{ order.id } a été prise en compte"
   end
 end
 
