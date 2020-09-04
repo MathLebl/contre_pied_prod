e55db631e333ae21df2268ef8c6563c081d01ff1
MathLebl <leblond.mathieu.kesemale@gmail.com>
2020-09-04 16:38:38 +0200

    fix host url

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,11 +4,9 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def default_url_options
-    # { host: ENV["DOMAIN"] || "localhost:3000" || "#{"HEROKU_APP_NAME"}.herokuapp.com" }
-    return ENV["DOMAIN"] if ENV["DOMAIN"].present?
-    return "#{ENV["HEROKU_APP_NAME"]}.herokuapp.com" if ENV["HEROKU_APP_NAME"].present?
-    "localhost:3000"
+    {:host => default_url_host}
   end
+
   # Création du panier si celui ci n'existe pas ou est vide
   def cart
     unless session[:cart]
@@ -20,6 +18,14 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :admin, :name, :first_name])
+  end
+
+  private
+
+  def default_url_host
+    return ENV["DOMAIN"] if ENV["DOMAIN"].present?
+    return "#{ENV["HEROKU_APP_NAME"]}.herokuapp.com" if ENV["HEROKU_APP_NAME"].present?
+    "localhost:3000"
   end
 end
 
