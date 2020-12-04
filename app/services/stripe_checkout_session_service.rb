09f86f06c8855b25b906a5d3fe22bcfcaff9688c
MathLebl <leblond.mathieu.kesemale@gmail.com>
2020-12-04 18:02:20 +0100

    stock flow

--- app/services/stripe_checkout_session_service.rb
+++ app/services/stripe_checkout_session_service.rb
@@ -2,5 +2,8 @@ class StripeCheckoutSessionService
   def call(event)
     order = Order.find_by(checkout_session_id: event.data.object.id)
     order.update(state: 'paid')
+    order.products.each do |product|
+      product.stock -= 1
+    end
   end
 end
