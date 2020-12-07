b8a16b681867e5b3524a0bbfcffa12c9341653b7
MathLebl <leblond.mathieu.kesemale@gmail.com>
2020-12-07 19:36:02 +0100

    photo rab

--- app/models/product.rb
+++ app/models/product.rb
@@ -13,5 +13,17 @@ class Product < ApplicationRecord
   has_one_attached :product_image3
   has_one_attached :product_image4
   has_one_attached :product_image5
+  has_one_attached :product_image6
+  has_one_attached :product_image7
+  has_one_attached :product_image8
+  has_one_attached :product_image9
+  has_one_attached :product_image10
+  has_one_attached :product_image11
+  has_one_attached :product_image12
+  has_one_attached :product_image13
+  has_one_attached :product_image14
+  has_one_attached :product_image15
+
+
 
 end
