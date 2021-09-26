a41492228863db0132e39a8365f95b6783d83f72
Mathieu Leblond <leblond.mathieu.kesemale@gmail.com>
2021-09-26 23:05:40 +0200

    correction, at last ?

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'lucas@contrepiedproductions.fr'
+  default from: 'contrepiedprod@gmail.com'
   layout 'mailer'
 end
--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -117,7 +117,7 @@ Rails.application.configure do
   :port                 => 587,
   :user_name            => ENV['gmail_username'],
   :password             => ENV['gmail_password'],
-  :authentication       => "plain"
+  :authentication       => "plain",
   :enable_starttls_auto => true
   }
   # config.action_mailer.smtp_settings = {
